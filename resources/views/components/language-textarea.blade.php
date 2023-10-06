d09ac9b177b44e97917b850cda62cec7d5d71b35
Ayoub CHAHIR <ayoub.chahir@newcogroup.com>
2023-10-06 15:29:12 +0100

    make textarea white

--- resources/views/components/language-textarea.blade.php
+++ resources/views/components/language-textarea.blade.php
@@ -4,5 +4,5 @@
     'placeholder' => ''
 ])
 
-<textarea type="text" placeholder="{{$placeholder}}" {{ $attributes->class(['w-full h-72 p-4 border-transparent']) }}
+<textarea type="text" placeholder="{{$placeholder}}" {{ $attributes->class(['w-full h-72 bg-white p-4 border-transparent']) }}
            id='{{$id}}'>{{ $text }}</textarea>
